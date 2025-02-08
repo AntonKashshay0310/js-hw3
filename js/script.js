a3264c2dbb91b9e05aa9d80e146b39864742cb27
AntonKashshay0310 <antosha.kashshay@gmail.com>
2025-02-08 15:31:19 +0200

    add

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// 1
+const result = 5 + 5 + '5';
+console.log(`Результат:${result} Тип змінної:${typeof result}`);result 
+// 2
+const email = 'antosha.kashshay@gmail.com'
+console.log(`Cимволів:${email.length} Наявність @:${email.includes(`@`)}`);
+// 3
+const firstPart = "My";
+const secondPart = "name";
+const thirdPart = "is";
+const fullname = `${firstPart}, ${secondPart}, ${thirdPart} Viktor.`;
+// 4
+const userName = "Олександро";
+const payment = "300"
+alert(`Дякуємо, ${userName}! До сплати ${payment} гривень.`)
